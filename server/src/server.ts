311dbccf5b6aeee4c95c299bec4e5e8bed4fe525
thiagoasb <thiagoasbezerra@gmail.com>
2020-06-03 17:55:48 -0300

    configuração de ambiente

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        'Diego',
+        'Cleiton',
+        'Robson',
+        'Daniel',
+        'Test'
+    ]);
+});
+
+app.listen(3333);
